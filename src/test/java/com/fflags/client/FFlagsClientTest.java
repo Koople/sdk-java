abc72f1073b5e8c693029e2a95ace573602de59e
David Gomez <ldavid.gomez@gmail.com>
2020-02-01 11:11:46 +0100

    Rename test

--- src/test/java/com/fflags/client/FFlagsClientTest.java
+++ src/test/java/com/fflags/client/FFlagsClientTest.java
@@ -5,18 +5,26 @@ import org.junit.Test;
 
 import java.io.IOException;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 
 public class FFlagsClientTest extends EasyMockSupport {
 
     @Test
-    public void constructorThrowsExceptionWhenNullSdkKKey() {
+    public void should_throws_exception_when_sdk_key_is_null() {
         try(FFClient ffClient = new FFClient(null)) {
             fail("expected null pointer exception");
         } catch (NullPointerException | IOException e) {
             assertEquals("Sdk key must not be null", e.getMessage());
+        }
+    }
+
+    @Test
+    public void should_return_a_validate_client_when_key_is_correct() {
+        try(FFClient ffClient = new FFClient("validKey")) {
+            assertNotNull(ffClient);
+        } catch (NullPointerException | IOException e) {
+            fail("expected new client");
         }
     }
 }
