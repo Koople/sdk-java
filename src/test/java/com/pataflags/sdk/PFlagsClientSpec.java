c73c27d89e72a3a8452b4524512fce1e463eda47
David Serrano <davsertor@gmail.com>
2020-05-19 19:55:51 +0200

    add client tests

--- src/test/java/com/pataflags/sdk/PFlagsClientSpec.java
+++ src/test/java/com/pataflags/sdk/PFlagsClientSpec.java
@@ -21,12 +21,39 @@ import static org.junit.Assert.*;
 @RunWith(EasyMockRunner.class)
 public class PFlagsClientSpec extends EasyMockSupport {
 
-    User user = new User("pedro", new HashMap<String, Value<?>>() {{
+    Segment spainAdults = new Segment(new ArrayList<Rule>() {{
+        add(new Rule(new ArrayList<Statement>(){{
+            add(new Statement("country", Operator.equals, new ArrayList<Value<?>>() {{
+                add(new StringValue("spain"));
+            }}));
+            add(new Statement("age", Operator.greaterThanOrEquals, new ArrayList<Value<?>>() {{
+                add(new IntValue(18));
+            }}));
+        }}));
+    }});
+
+    Segment eeuuAdults = new Segment(new ArrayList<Rule>() {{
+        add(new Rule(new ArrayList<Statement>(){{
+            add(new Statement("country", Operator.equals, new ArrayList<Value<?>>() {{
+                add(new StringValue("eeuu"));
+            }}));
+            add(new Statement("age", Operator.greaterThanOrEquals, new ArrayList<Value<?>>() {{
+                add(new IntValue(21));
+            }}));
+        }}));
+    }});
+
+    User user = new User("pedrito", new HashMap<String, Value<?>>() {{
         put("country", new StringValue("spain"));
-        put("age", new IntValue(50));
+        put("age", new IntValue(18));
     }});
 
     private List<Feature> features = new ArrayList<Feature>() {{
+        add(new Feature("disabledForAll", FeatureTargeting.disabled));
+        add(new Feature("enabledForHimself", FeatureTargeting.segment, new ArrayList<String>() {{ add("pedrito"); }}, new ArrayList<Segment>()));
+        add(new Feature("enabledForOther", FeatureTargeting.segment, new ArrayList<String>() {{ add("paco"); }}, new ArrayList<Segment>()));
+        add(new Feature("enabledForSpainAdults", FeatureTargeting.segment, new ArrayList<String>(), new ArrayList<Segment>() {{ add(spainAdults); }}));
+        add(new Feature("enabledForEeuuAdults", FeatureTargeting.segment, new ArrayList<String>(), new ArrayList<Segment>() {{ add(eeuuAdults); }}));
         add(new Feature("enabledForAll", FeatureTargeting.all));
     }};
 
@@ -90,6 +117,13 @@ public class PFlagsClientSpec extends EasyMockSupport {
         client.init();
         Map<String, Boolean> evaluation = client.evaluate(user);
 
-        assertEquals(evaluation, new HashMap<String, Boolean>() {{ put("enabledForAll", true); }});
+        assertEquals(evaluation, new HashMap<String, Boolean>() {{
+            put("disabledForAll", false);
+            put("enabledForHimself", true);
+            put("enabledForOther", false);
+            put("enabledForSpainAdults", true);
+            put("enabledForEeuuAdults", false);
+            put("enabledForAll", true);
+        }});
     }
 }
